253c34e5b31889f3d8763f061faad9a296658a01
Justin Ho <jho1@stevens.edu>
2021-04-29 18:43:00 -0400

    merge

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import packet_switching as ps
 import circuit_switching as cs
 
+MEAN_ARRIVAL_TIME = [10, 25, 50, 75, 100]
+TRANSMISSION_TIME = [10, 25, 50, 75, 100]
+
 if __name__ == '__main__':
-    # PACKET SWITCHING
-    sim_duration_hours = 1
-    sim_duration_ms = sim_duration_hours * 3600000
+    for trans in TRANSMISSION_TIME:
+        print('\n' + '=' * 60)
+        print('TRANSMISSION TIME: ' + str(trans))
 
-    simulation_ps = ps.PacketSwitchingSim(sim_duration_ms, mean_data_arrival_time=50, transmission_time=80)
-    # simulation.view_nodes()
-    simulation_ps.run_sim()
-    print('\n' + '-'*60)
+        for arr in MEAN_ARRIVAL_TIME:
+            print('\n' + '~' * 60)
+            print("MEAN ARRIVAL TIME: " + str(arr))
+
+            # PACKET SWITCHING
+            sim_duration_hours = 1
+            sim_duration_ms = sim_duration_hours * 3600000
+
+            simulation_ps = ps.PacketSwitchingSim(sim_duration_ms, mean_data_arrival_time=arr, transmission_time=trans)
+            # simulation.view_nodes()
+            simulation_ps.run_sim()
+            print('\n' + '-'*60)
 
 
-    # CIRCUIT SWITCHING
-    sim_duration_hours = 1
-    sim_duration_ms = sim_duration_hours * 3600000
+            # CIRCUIT SWITCHING
+            sim_duration_hours = 1
+            sim_duration_ms = sim_duration_hours * 3600000
 
-    simulation_cs = cs.CircuitSwitchingSim(sim_duration_ms, mean_data_arrival_time=50, transmission_time=80)
-    # simulation_cs.view_nodes()
-    simulation_cs.run_sim()
+            simulation_cs = cs.CircuitSwitchingSim(sim_duration_ms, mean_data_arrival_time=arr, transmission_time=trans)
+            # simulation_cs.view_nodes()
+            simulation_cs.run_sim()
